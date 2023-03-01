9decb73a1bebd15a5ee159f09d3a391dfdf29058
Gaëtan <gaetan.moreau@agence-gw.com>
2023-03-01 10:43:56 +0100

    Add a basketlist and move basket content to the basketlist file

--- projet-svelte/src/lib/Basket.svelte
+++ projet-svelte/src/lib/Basket.svelte
@@ -1,23 +1,11 @@
 <script>
-  import { slide } from "svelte/transition";
   import { panier, emptyBasket } from "../store.js";
-  import Item from "./Item.svelte";
+  import BasketList from "./BasketList.svelte";
 
   let basketListIsOpen = false;
+  let totalPrice = 0;
 
   $: totalQuantity = $panier.reduce((acc, item) => acc + item.quantity, 0);
-  $: totalPrice = parseFloat(
-    $panier.reduce((acc, item) => acc + item.priceOfGames, 0).toFixed(2)
-  );
-
-  function proceedToCheckout() {
-    console.log("Votre panier est vide");
-    if (totalQuantity > 0) {
-      emptyBasket();
-      basketListIsOpen = false;
-      console.log("Commande validé, amusez vous bien !");
-    }
-  }
 </script>
 
 <div
@@ -33,32 +21,7 @@
 </div>
 
 {#if basketListIsOpen}
-  <div class="basket__list" transition:slide>
-    <div class="basketList__header">
-      <h2>Votre panier</h2>
-      <span
-        class="bastket__close"
-        on:click={() => (basketListIsOpen = false)}
-        on:keypress={() => (basketListIsOpen = false)}>X</span
-      >
-    </div>
-    <div>
-      {#each $panier as item}
-        <Item
-          game={item.game}
-          quantity={item.quantity}
-          isInCart={true}
-          let:quantity
-        >
-          <div class="game__price">{item.priceOfGames.toFixed(2)} €</div>
-        </Item>
-      {/each}
-    </div>
-    <div class="basket__order">
-      <p>Total : <strong>{totalPrice} €</strong></p>
-      <button on:click={() => proceedToCheckout()}>Procéder au paiement</button>
-    </div>
-  </div>
+  <BasketList bind:basketListIsOpen totalQuantity bind:totalPrice />
 {/if}
 
 <style>
@@ -79,43 +42,6 @@
   }
   .shopping__bag {
     width: 1.6rem;
-  }
-  .basket__list {
-    position: fixed;
-    top: 0px;
-    bottom: 0px;
-    width: 300px;
-    right: 0;
-    background-color: #fff;
-    padding: 20px;
-    font-size: 1.6rem;
-    overflow-y: scroll;
-    overflow-x: hidden;
-  }
-  .bastket__close {
-    cursor: pointer;
-  }
-  .basketList__header {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-    border-bottom: 1px solid;
-  }
-  .basket__order {
-    display: flex;
-    flex-direction: column;
-  }
-  .basket__order button {
-    font-size: 1.4rem;
-    background-color: #0e3a53;
-    border-radius: 0;
-    border: 0;
-    padding: 10px 16px;
-    margin-top: 20px;
-  }
-  .basket__order button:hover {
-    background-color: #000;
   }
   .shopping__bag__container {
     position: relative;
