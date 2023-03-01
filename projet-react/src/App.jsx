a26962f686743965020b913d83ace678b47ffded
Gaëtan <gaetan.moreau@agence-gw.com>
2023-03-01 16:06:32 +0100

    Add header and app

--- projet-react/src/App.jsx
+++ projet-react/src/App.jsx
@@ -1,9 +1,21 @@
-import { useState } from "react";
 import Header from "./lib/Header";
+import Item from "./lib/Item";
 import "./App.css";
+import games from "./data/games.json"
 
 function App() {
-  return <Header></Header>;
+  return (
+    <>
+      <Header></Header>
+      <main>
+        <section className="game__header">Buy your games cheaper here !</section>
+        <section className="games__container">
+          <div className="container">
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
 
 export default App;
